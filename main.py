a753455af03adc4ffdfd55a440c40eb2169d3e91
Betim Pantina <pantinabetim@gmail.com>
2023-11-27 20:17:55 +0100

    last lesson of dev-boot project

---
+++ main.py
@@ -0,0 +1,34 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} found in the document")
+    for char,value in chars_dict.items():
+        if char.isalpha():
+            print(f"The {char} character was found {value} times")
+    print("--- End report ---")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
